5d18200bc71928d32ceab6ef9cb77d44568fd8be
Lucas Bitencourt <lblukz@aol.com>
2022-03-21 17:34:00 -0300

    Requisito 8 concluído

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ let quadroDePixel = document.getElementById('pixel-board');
 let corPreto = document.getElementById('cor1');
 let corPaleta = document.querySelectorAll('.color');
 
+
 // Requisito 4
 for (let index = 1; index <=5; index ++) {
     let linhaDePixel = document.createElement('div');
     quadroDePixel.appendChild(linhaDePixel);
     for (index2 = 1; index2 <=5; index2++) {
-        let pixel = document.createElement('span');
-        pixel.className = 'pixel';
-        linhaDePixel.appendChild(pixel);
+        let criaPixel = document.createElement('span');
+        criaPixel.className = 'pixel';
+        linhaDePixel.appendChild(criaPixel);
     }
 }
 
@@ -28,4 +29,17 @@ function selecionaCor (acao) {
     acao.target.classList.add('selected');
 }
 
+// Requisito 8
+let pixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixel.length; index++) {
+    pixel[index].addEventListener('click', pinta);
+}
 
+function pinta (cor) {
+    const selecao = document.querySelector('.selected');
+    const dadosSelecao = window.getComputedStyle(selecao, null);
+    let corSelecionada = dadosSelecao.getPropertyValue('background-color');
+    cor.target.style.backgroundColor = corSelecionada;
+    console.log(selecao);
+}
+//console.log(pixel);
